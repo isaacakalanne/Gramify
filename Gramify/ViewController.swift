46989d2d6834d285b0034ae476bed8ff1838fdf7
Isaac <isaacakalanne@gmail.com>
2020-01-29 12:56:04 +0000

    Initial Commit

---
+++ Gramify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gramify
+//
+//  Created by Isaac Akalanne on 29/01/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
