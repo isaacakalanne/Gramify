805e0d6a990bda3c15dc34ca9058b348aeab6160
Isaac <isaacakalanne@gmail.com>
2020-01-30 20:20:39 +0000

    Created a class called 'NetworkProcessor'. This class contains a function called 'downloadJSONFromURL' which downloads JSON data from an Album of images on Imgur using the Imgur API.

---
+++ Gramify/NetworkProcessor.swift
@@ -0,0 +1,51 @@
+//
+//  NetworkProcessor.swift
+//  Gramify
+//
+//  Created by Isaac Akalanne on 30/01/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import Foundation
+
+class NetworkProcessor {
+    
+    lazy var configuration: URLSessionConfiguration = URLSessionConfiguration.default
+    lazy var session: URLSession = URLSession(configuration: self.configuration)
+
+    let url: URL
+
+    init(url: URL) {
+        self.url = url
+    }
+    
+    typealias JSONDictionaryHandler = (([String : Any]?) -> Void)
+    
+    func downloadJSONFromURL(_ completion: @escaping JSONDictionaryHandler) {
+        
+        var request = URLRequest(url: self.url)
+        request.setValue("Client-ID ab89787499de1c4", forHTTPHeaderField: "Authorization")
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if error == nil {
+                
+                if let httpResponse = response as? HTTPURLResponse {
+                    switch httpResponse.statusCode {
+                    case 200:
+                        if let data = data {
+                            print(data)
+                        }
+                    default:
+                        print("HTTP Response Code: \(httpResponse.statusCode)")
+                    }
+                }
+                
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+        dataTask.resume()
+    }
+    
+}
