91283862f1a03a63fe88ab91b8546aea35d17cd7
Isaac <isaacakalanne@gmail.com>
2020-02-02 18:47:14 +0000

    Created ImageTableViewCell class, for the cells in the ImageListTableViewController, and formatted a UITableViewCell for the file in Main.storyboard.

---
+++ Gramify/ImageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ImageTableViewCell.swift
+//  Gramify
+//
+//  Created by Isaac Akalanne on 02/02/2020.
+//  Copyright © 2020 Believe And Succeed Apps. All rights reserved.
+//
+
+import UIKit
+
+class ImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
